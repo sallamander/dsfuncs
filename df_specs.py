d82e06f66a5360d6390c8201825aeac1a3dfef5b
sallamander <ssall@alumni.nd.edu>
2015-08-01 19:40:20 -0600

    Finish intial version of df_hist in plotting_funcs

--- df_specs.py
+++ df_specs.py
@@ -2,3 +2,7 @@ import pandas as pd
 
 def load_data(filename):
 	df = pd.read_csv(filename) 
+	return df
+
+def get_columns(df, column_list): 
+	return df[column_list]
--- plotting_funcs.py
+++ plotting_funcs.py
@@ -3,46 +3,88 @@
 # data frames.) Others aren't really anything special - I just got tired of typing plt.feature
 # any time I wanted to set a feature. 
 
+import seaborn as sns
 import matplotlib.pyplot as plt
+import math
 
-def plt_specs(xlab = None, ylab = None, title = None, save_title = None, 
+def plt_specs(ax = None, xlab = None, ylab = None, title = None, save_title = None, 
 			  sub_plot = None, xlim = None, ylim = None, 
-			  legend = None, tight_layout = None): 
+			  legend = None, tight_layout = None, grid_bool = None): 
 	# If the parameter was passed in, set it for that plot. Otherwise 
-	# do nothing. 
-	if xlab: 
-		plt.xlabel(xlab)
-	if ylab: 
-		plt.ylabel(ylab)
-	if title: 
-		plt.title(title)
-	if xlim: 
-		plt.xlim(xlim)
-	if ylim: 
-		plt.ylim(ylim)
+	# do nothing. The first parameter we check for is the axis = if the axis was 
+	# passed, then we call the plt_axis function and plot everything on that axis. 
+	# Then we come back and plot the more general plt things (tight layout and save_title. 
+	if ax: 
+		plt_axis(ax = ax, xlab = xlab, ylab = ylab, title = title, xlim = xlim, 
+				 ylim =ylim, grid_bool = grid_bool)
+	else: 
+		if xlab: 
+			plt.xlabel(xlab)
+		if ylab: 
+			plt.ylabel(ylab)
+		if title: 
+			plt.title(title)
+		if xlim: 
+			plt.xlim(xlim)
+		if ylim: 
+			plt.ylim(ylim)
+	if grid_bool is not None and ax is None: 
+		sub = plt.subplot(1, 1,1)
+		sub.grid(grid_bool)
 	if legend:
-		plt.legend() 
+		plt.legend(loc = 'best') 
 	if tight_layout: 
 		plt.tight_layout()
-	### Make sure this is the last thing!
+	# This needs to be the last thing, or else anything rendered after this won't save. 
 	if save_title: 
 		plt.savefig(save_title)
 
-def plt_axis(ax = None, xlab = None, ylab = None, title = None, xlim = None, ylim = None, grid = None): 
+def plt_axis(ax = None, xlab = None, ylab = None, title = None, xlim = None, ylim = None, grid_bool = None, legend = None): 
 	# If the parameter was passed in, then set it for the axis that is passed in. Note that 
 	# an axis has to be passed in. It's not explicit, since I have put a default in as None - I'm 
 	# using that to let the user know that an axis has to be passed in. 
 	if ax is None: 
 		raise Exception('No axis passed in... if there isnt one, try using plt_specs instead.')
 	if xlab: 
-		ax.xlabel(xlab)
+		ax.set_xlabel(xlab)
 	if ylab: 
-		ax.ylabel(ylab)
+		ax.set_ylabel(ylab)
 	if title: 
 		ax.set_title(title)
 	if xlim: 
-		ax.xlim(xlim)
+		ax.set_xlim(xlim)
 	if ylim: 
-		ax.ylim(ylim)
-	if grid: 
-		ax.grid(grid)
+		ax.set_ylim(ylim)
+	if legend: 
+		ax.legend = True
+	if grid_bool is not None: 
+			ax.grid(grid_bool)
+
+def df_hist(df, bins = 20, kde = False, norm_hist = True, columns = None, gridsize = None, xlim = None, 
+			ylim = None, xlab = None, ylab = None, legend = None, hist_kws = None, grid_bool = None,
+			title = None): 
+	# We'll iterate through the columns the user passes in and plot a histogram for 
+	# each column. If no columns were passed in, then plot a histogram for all of them. 
+	plot_columns = df.columns if columns is None else columns
+	if gridsize is None: 
+		# If the gridsize is empty, then what we'll do is to simply make an n x n grid, where
+		# n is equal to the sqrt of the number of columns (take the ciel of that though). 
+		# It'll end up as square as possible.  The only issue with this is if there are only two
+		# columns - we end up with a 2 by 2 plot, but we really just want a 1 x 2. 
+		sqrt_columns = math.ceil(len(columns) ** 0.5)
+		gridsize = (sqrt_columns, sqrt_columns)
+		
+		if len(columns) == 2: 
+			gridsize = (1, 2)
+
+	for index, col in enumerate(plot_columns): 
+		# Set the title to the column name. I know type checking isn't a python thing, but if 
+		# somebody wants to pass in the column integer versus name, I want to print the actual
+		# column name, and not just the index number. 
+		title = df.columns[index] if type(col) is int else col
+		# Grab the appropriate column of the data. Using ix 
+		data = df.ix[:, col]
+		sub = plt.subplot(gridsize[0], gridsize[1], index + 1)
+		sns.distplot(data, hist = True, bins = bins, kde = kde, norm_hist = norm_hist, hist_kws = hist_kws)
+		plt_specs(ax = sub, xlim = xlim, ylim = ylim, xlab = xlab, ylab = ylab, title = title, legend = legend, 
+		 		  grid_bool = grid_bool)
