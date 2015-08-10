4d3496beb1f3fa1413c90dd006c045f3dd62c983
sallamander <ssall@alumni.nd.edu>
2015-08-09 22:58:38 -0600

    Add docstrings for data_minp.py

--- data_manip.py
+++ data_manip.py
@@ -3,12 +3,29 @@ import numpy as np
 from collections import defaultdict
 
 def change_type(df, columns, type): 
+	'''
+	Input: Data Frame, list of columns, list of types
+	Ouput: DataFrame with each column in list of columns changed to the type in the corresponding.
+	in list of types. 
+	'''
+
 	for column in columns: 
 		df[column] = df[column].astype(type)
 
 	return df
 
 def test_transforms(target, feature, df=None): 
+	''' 
+	Input: Column DataFrame/Numpy Array, Column DataFrame/Numpy Array
+	Output: Dictionary of possible transformations as keys and correlation values as values. 
+
+	This function runs through a number of different transformations that we could perform on our 
+	feature/X variable and calculates the pearson correlation between the target and transformed 
+	feature. It outputs a dictionary that holds the results of each transformation, as well as a 
+	key named 'best' that corresponds to the transformation that had the highest pearson correlation 
+	(in absolute value). 
+	'''
+
 	if df: 
 		target = df[target]
 		feature = df[feature]
