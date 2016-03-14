2f9218a8962b917767a935090c8c8b885eda7939
sallamander <ssall@alumni.nd.edu>
2016-03-13 21:19:29 -0400

    Initialize plot_binary_response func

--- plotting.py
+++ plotting.py
@@ -81,3 +81,23 @@ def _plot_continuous_var_dist(var_data, ax, show):
     
     if show: 
         plt.show()
+
+def plot_binary_response(df, response, ax, show): 
+    """Plot the percentage of a True/False binary response across
+    a categorical variable. 
+
+    Take the inputted DataFrame, and plot the binary response variable
+    percentages across cateogries of a categorical variable. For each 
+    category in the categorical variable, use the percent of True's as the 
+    height of the bar. Use the percent of the total obs. that that category 
+    makes up as the text above the bar. 
+
+    Args: 
+        df: Pandas DataFrame
+        response: str
+            Holds the column name of the response variable. 
+        ax: matplotlib.axes
+        show: bool
+            Boolean that holds whether or not to display the plot after.
+    """
+    pass
