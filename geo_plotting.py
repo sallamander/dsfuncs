c12e223f5515946f359a1c9c44a4cdfabf21475e
sallamander <ssall@alumni.nd.edu>
2016-03-14 17:32:11 -0400

    Initialize StateMap class

--- geo_plotting.py
+++ geo_plotting.py
@@ -33,3 +33,44 @@ class USMap():
                     lon_2 = -65., lon_0 = -97.5)
         self.geo_map.readshapefile(shapefile_path, self.geo_level)
 
+class Statemap(USMap): 
+    """This docstring will describe how to interact with the Statemap class. 
+
+    this class will take the instantiated usmap from the usmap class, and then 
+    take it down to the state level by restricting it to the inputted state. 
+
+    parameters: 
+    -----------
+        shapefile_path: str
+            holds a path to the shapefile that the map will be built on 
+            top of. 
+        geo_level: str
+            holds the geographical level of granularity (state, county, etc.)
+            that is in the inputted shapefile_path. 
+        state_name: str
+    """
+
+    fips_dict = {'Alabama': '01', 'Alaska': '02', 'Arizona': '04', 
+            'Arkansas': '05', 'California': '06', 'Colorado': '08', 
+            'Connecticut': '09', 'Deleware': '10', 
+            'District of Columbia': '11', 'Florida': '12', 'Georgia': '13', 
+            'Hawaii': '15', 'Idaho': '16', 'Illinois': '17', 'Indiana': '18', 
+            'Iowa': '19' , 'Kansas': '20', 'Kentucky': '21', 'Lousiana': '22',
+            'Maine': '23', 'Maryland': '24', 'Massachusetts': '25', 
+            'Michigan': '26', 'Minnesota': '27', 'Mississippi': '28', 
+            'Missouri': '29', 'Montana': '30', 'Nebraska': '31', 
+            'Nevada': '32', 'New Hampshire': '33', 'New Jersey': '34', 
+            'New Mexico': '35', 'New York': '36', 'North Carolina': '37', 
+            'North Dakota': '38', 'Ohio': '39', 'Oklahoma': '40', 
+            'Oregon': '41', 'Pennsylvania': '42', 'Rhode Island': '44', 
+            'South Carolina': '45', 'South Dakota': '46', 'Tennessee': '47', 
+            'Texas': '48', 'Utah': '49', 'Vermont': '50', 'Virgina': '51', 
+            'Washington': '53', 'West Virginia': '54', 'Wisconsin': '55', 
+            'Wyoming': '56'}
+
+    def __init__(self, shapefile_path, geo_level, state_name): 
+        pass
+        
+
+
+
